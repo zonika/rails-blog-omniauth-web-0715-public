286c9aec8b3e67362daaf4b316c65c4950a27656
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-09 12:04:48 -0400

    tests

---
+++ spec/features/omniauth_login_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../spec_helper.rb'
+
+describe 'user log in with omniauth', :type => :feature do
+
+  let(:auth) { OmniAuth.config.mock_auth[:github_user] }
+
+  it 'can log in with github' do 
+    visit '/auth/github'
+    expect(page).to have_content(auth["name"])
+  end
+
+  it 'can stay logged in, keeping session' do 
+    visit '/auth/github'
+    click_link 'New Post'
+    expect(page).to have_content(auth["name"])
+    expect(page.get_rack_session_key('user_id')).to_not eq(nil)
+  end
+
+  it 'can log out, clearing session' do 
+    visit '/auth/github'
+    expect(page).to have_content(auth["name"])
+    click_link 'Log Out'
+    expect(current_path).to eq('/')
+    expect(page).to have_content('Log In with Github')
+    expect(page.get_rack_session).to_not include('user_id')
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe User do
+
+  describe ".create_with_omniauth" do
+
+    let(:auth) { OmniAuth.config.mock_auth[:github_user] }
+
+    it 'initializes new users from github' do
+      user = User.create_with_omniauth(auth)
+      expect(user).to be_persisted
+      expect(user.provider).to eq(auth["provider"])
+      expect(user.uid).to eq(auth["uid"])
+      expect(user.name).to eq(auth["info"]["name"])
+    end
+
+  end
+
+end
